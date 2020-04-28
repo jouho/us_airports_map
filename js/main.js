106d9a53e5a5efcd02b731453d8a148601f8f8b0
Jou Ho <jouh@uw.edu>
2020-04-27 22:01:05 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // 1. Create a map object.
 var mymap = L.map('map', {
-    center: [54.5260, -105.2551],
-    zoom: 4,
-    maxZoom: 10,
-    minZoom: 3,
-    detectRetina: true});
+  center: [54.5260, -105.2551],
+  zoom: 4,
+  maxZoom: 10,
+  minZoom: 3,
+  detectRetina: true
+});
 
 // 2. Add a base map.
 L.tileLayer('https://cartocdn_{s}.global.ssl.fastly.net/base-antique/{z}/{x}/{y}.png').addTo(mymap);
@@ -18,81 +19,98 @@ var colors = chroma.scale('Set1').mode('lch').colors(9);
 
 // 5. dynamically append style classes to this page. This style classes will be used for colorize the markers.
 for (i = 0; i < 9; i++) {
-    $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
+  $('head').append($("<style> .marker-color-" + (i + 1).toString() + " { color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
 }
 
 // Get GeoJSON and put on it on the map when it loads
-cellTowers= L.geoJson.ajax("assets/airports.geojson",{
-    // assign a function to the onEachFeature parameter of the cellTowers object.
-    // Then each (point) feature will bind a popup window.
-    // The content of the popup window is the value of `feature.properties.company`
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.AIRPT_NAME);
-    },
-    pointToLayer: function (feature, latlng) {
-        var id = 0;
-        if (feature.properties.CNTL_TWR == "Y") { id = 0; }
-        else { id = 1;} // "N"
-        return L.marker(latlng, {icon: L.divIcon({className: 'fas fa-plane-departure marker-color-' + (id + 1).toString() })});
-    },
-    attribution: 'Airport Data &copy; DATA.GOV | U.S. counties &copy; Mike Bostock | Base Map &copy; CARTO | Made By Jou Ho'
+cellTowers = L.geoJson.ajax("assets/airports.geojson", {
+  // assign a function to the onEachFeature parameter of the cellTowers object.
+  // Then each (point) feature will bind a popup window.
+  // The content of the popup window is the value of `feature.properties.company`
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup(feature.properties.AIRPT_NAME);
+  },
+  pointToLayer: function(feature, latlng) {
+    var id = 0;
+    if (feature.properties.CNTL_TWR == "Y") {
+      id = 0;
+    } else {
+      id = 1;
+    } // "N"
+    return L.marker(latlng, {
+      icon: L.divIcon({
+        className: 'fas fa-plane-departure marker-color-' + (id + 1).toString()
+      })
+    });
+  },
+  attribution: 'Airport Data &copy; DATA.GOV | U.S. counties &copy; Mike Bostock | Base Map &copy; CARTO | Made By Jou Ho'
 }).addTo(mymap);
 
 // 6. Set function for color ramp
 colors = chroma.scale('OrRd').colors(5); //colors = chroma.scale('OrRd').colors(5);
 
 function setColor(density) {
-    var id = 0;
-    if (density > 18) { id = 4; }
-    else if (density > 13 && density <= 18) { id = 3; }
-    else if (density > 10 && density <= 13) { id = 2; }
-    else if (density > 5 &&  density <= 10) { id = 1; }
-    else  { id = 0; }
-    return colors[id];
+  var id = 0;
+  if (density > 18) {
+    id = 4;
+  } else if (density > 13 && density <= 18) {
+    id = 3;
+  } else if (density > 10 && density <= 13) {
+    id = 2;
+  } else if (density > 5 && density <= 10) {
+    id = 1;
+  } else {
+    id = 0;
+  }
+  return colors[id];
 }
 
 
 // 7. Set style function that sets fill color.md property equal to cell tower density
 function style(feature) {
-    return {
-        fillColor: setColor(feature.properties.count),
-        fillOpacity: 0.4,
-        weight: 2,
-        opacity: 1,
-        color: '#b4b4b4',
-        dashArray: '4'
-    };
+  return {
+    fillColor: setColor(feature.properties.count),
+    fillOpacity: 0.4,
+    weight: 2,
+    opacity: 1,
+    color: '#b4b4b4',
+    dashArray: '4'
+  };
 }
 
 // 8. Add county polygons
 // create counties variable, and assign null to it.
 var states = null;
 states = L.geoJson.ajax("assets/us-states.geojson", {
-    style: style
+  style: style
 }).addTo(mymap);
 
 // 9. Create Leaflet Control Object for Legend
-var legend = L.control({position: 'topright'});
+var legend = L.control({
+  position: 'topright'
+});
 
 // 10. Function that runs when legend is added to map
-legend.onAdd = function () {
+legend.onAdd = function() {
 
-// Create Div Element and Populate it with HTML
-var div = L.DomUtil.create('div', 'legend');
-div.innerHTML += '<b># Airports</b><br />';
-div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
-div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
-div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
-div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
-div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
-div.innerHTML += '<hr><b>Has Air Traffic Control Tower<b><br />';
-div.innerHTML += '<i class="fas fa-plane-departure marker-color-1"></i><p> Yes</p>';
-div.innerHTML += '<i class="fas fa-plane-departure marker-color-2"></i><p> No</p>';
-// Return the Legend div containing the HTML content
-return div;
+  // Create Div Element and Populate it with HTML
+  var div = L.DomUtil.create('div', 'legend');
+  div.innerHTML += '<b># Airports</b><br />';
+  div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>19+</p>';
+  div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>14-18</p>';
+  div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>11-13</p>';
+  div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 6-10</p>';
+  div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 5</p>';
+  div.innerHTML += '<hr><b>Has Air Traffic Control Tower<b><br />';
+  div.innerHTML += '<i class="fas fa-plane-departure marker-color-1"></i><p> Yes</p>';
+  div.innerHTML += '<i class="fas fa-plane-departure marker-color-2"></i><p> No</p>';
+  // Return the Legend div containing the HTML content
+  return div;
 };
 
 // 11. Add a legend to map
 legend.addTo(mymap);
 
-L.control.scale({position: 'bottomleft'}).addTo(mymap);
+L.control.scale({
+  position: 'bottomleft'
+}).addTo(mymap);
